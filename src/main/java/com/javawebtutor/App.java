ee7ce1b75b207b4eec3ce3e0f9699ba384953e0d
Nitin Shukla <nitincontactme@gmail.com>
2019-08-21 11:52:58 +0530

    Added employee

--- src/main/java/com/javawebtutor/App.java
+++ src/main/java/com/javawebtutor/App.java
@@ -1,13 +1,35 @@
 package com.javawebtutor;
 
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private static EntityManager em;
+ 
+    public static void main(String[] args) {
+ 
+        EntityManagerFactory emf = Persistence
+                .createEntityManagerFactory("EmployeePU");
+        em = emf.createEntityManager();
+ 
+        createEmployee(1, "Ravi", "Raj", "Textile");
+        createEmployee(2, "Amit", "Raj", "IT");
+        createEmployee(3, "Nitish", "Kumar", "Marketing");
+ 
+    }
+ 
+    private static void createEmployee(int id, String firstName,
+            String lastName, String dept) {
+        em.getTransaction().begin();
+        Employee emp = new Employee(id, firstName, lastName, dept);
+        em.persist(emp);
+        em.getTransaction().commit();
     }
 }
